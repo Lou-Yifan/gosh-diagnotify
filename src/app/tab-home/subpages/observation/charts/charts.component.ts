e2abbb34fd49aae742ddfafad5ace5551e1e13bd
Lou-Yifan <yifan.lou@ucl.ac.uk>
2020-08-11 02:14:47 +0800

    charts

--- src/app/tab-home/subpages/observation/charts/charts.component.ts
+++ src/app/tab-home/subpages/observation/charts/charts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -7,13 +8,132 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charts.component.scss'],
 })
 export class ChartsComponent implements OnInit {
+  
+  @ViewChild('barChart_week', {static: true}) barChart_week;
+  @ViewChild('barChart_month', {static: true}) barChart_month;
+  @ViewChild('barChart_year', {static: true}) barChart_year;
+  @ViewChild('barChart_all', {static: true}) barChart_all;
+  
   segment_chart = "chart_week";
+  bars_week: any;
+  bars_month: any;
+  bars_year: any;
+  bars_all: any;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.CreateBarChart_week();
+    this.CreateBarChart_month();
+    this.CreateBarChart_year();
+    this.CreateBarChart_all();
+  }
+
+  ionViewDidEnter() {
+    //this.CreateBarChart();
+  }
 
   segmentChanged_chart(ev){
     console.log(ev);
+  }
+
+  CreateBarChart_week(){
+    this.bars_week = new Chart(this.barChart_week.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  CreateBarChart_month(){
+    this.bars_month = new Chart(this.barChart_month.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  CreateBarChart_year(){
+    this.bars_year = new Chart(this.barChart_year.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  CreateBarChart_all(){
+    this.bars_all = new Chart(this.barChart_all.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 }
